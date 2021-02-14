f73e2682b50ed4536626a91c8d8884d7a2797db8
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-02-14 10:23:25 +0100

    extended style example

--- R/style_plot.R
+++ R/style_plot.R
@@ -2,10 +2,10 @@
 #' 
 #' The \code{style_plot} function is used to create graphical styles for a single-case plot
 #' 
-#' @param style Predefined styles.
+#' @param style A character string or a vector of character strings with predefined styles.
 #' @param ... Further arguments passed to the plot command.
 #' 
-#' @return Returns a list to be provided or the style argument of the plot function.
+#' @return Returns a list to be provided for the style argument of the \code{\link{plot.scdf}} function.
 #' @return \itemize{ 
 #' \item\code{fill} If set, the area under the line is
 #' filled with the given color (e.g., \code{fill = "tomato"}). Use the standard
@@ -22,13 +22,16 @@
 #' \item\code{annotations = list(pos = 3, col =
 #' "brown", round = 1)} adds scores rounded to one decimal above the data point
 #' in brown color to the plot.
+#' \item\code{"names"} A list of parameters defining the depiction of phase names (e.g. \code{names = list(cex = 0.8, col = "red", side = 1)}: cex for size, col for color, and side for position). See \code{\link{mtext}} for more details. 
 #' \item\code{"lwd"} Width of the plot line. Default is \code{lwd = 2}.
 #' \item\code{"pch"} Point type. Default is \code{pch = 17} (triangles). Other options
 #' are for example: 16 (filled circles) or "A" (uses the letter A).
 #' \item\code{"main"} Main title of the plot.
 #' \item\code{"mai"} Sets the margins of the plot.
 #' \item\code{"bty"} Shape of the frame surrounding the inner plot
-#' \item\code{"fill.bg"} Backgroundcolor of the plot.
+#' \item\code{"fill.bg"} Background color of the plot.
+#' If a vector is provided, these colors will be assigned to phases 
+#' (each phase name becomes a color).
 #' \item\code{"grid"} Color of a grid.
 #' \item\code{"text.ABlag"} Text displayed between phases.
 #' \item\code{"cex.axis"} Size of the axis annotations
@@ -52,7 +55,9 @@
 #' newstyle$text.ABlag <- c("START","END")
 #' newstyle$col.dots <- ""
 #' newstyle$annotations <- list(cex = 0.6, col = "grey10", offset = 0.4)
-#' plot(exampleABC, style = newstyle)
+#' newstyle$names <- list(cex = 0.8, col = "blue", side = 1, adj = 1, line = -1, at = 31)
+#' newstyle$fill.bg <- c("grey99", "grey95", "grey90")
+#' plot(exampleABC, style = newstyle, main = "Example Plot")
 #' 
 #' @export
 style_plot <- function(style = "default", ...) {
