ae6663d77efb2d50108adb5ff4f5eba92049b52e
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-06-08 12:58:37 +0200

    allow setting dvar pvar and mvar

--- R/scdf-functions.R
+++ R/scdf-functions.R
@@ -1,25 +1,43 @@
 #' Combine single-case data frames into a multiple case study
 #'
 #' @param ... scdf objects
+#' @param dvar Character string. Name of the dependent variable. Defaults to the dependent variable of the first case provided.
+#' @param pvar Character string. Name of the phase variable. Defaults to the phase variable of the first case provided.
+#' @param mvar Character string. Name of the measurement-time variable. Defaults to the measurement-time variable of the first case provided.
 #'
-#' @return A scdf
+#' @return A scdf. If not set differently, the attributes of this scdf are copied from the first scdf provided (i.e the first argument of the function). 
 #' 
 #' @export
-combine <- function(...) {
+combine <- function(..., dvar = NULL, pvar = NULL, mvar = NULL) {
   scdfs <- list(...)
   
   source_attr <- attributes(scdfs[[1]])
-
+  
   case_names <- unlist(lapply(scdfs, names))
    
   data <- unlist(scdfs, recursive = FALSE)
-  attributes(data) <- .defaultAttributesSCDF()
+  
+  #attributes(data) <- .defaultAttributesSCDF()
 
-  if (!is.null(source_attr[[.opt$scdf]])) attr(data, .opt$scdf) <- source_attr[[.opt$scdf]]
+  #if (!is.null(source_attr[[.opt$scdf]])) attr(data, .opt$scdf) <- source_attr[[.opt$scdf]]
+  attributes(data) <- source_attr
+  #attr(data, .opt$scdf) <- source_attr[[.opt$scdf]]
+  
+  if (!is.null(dvar)) scdf_attr(data, .opt$dv) <- dvar
+  if (!is.null(mvar)) scdf_attr(data, .opt$mt) <- mvar
+  if (!is.null(pvar)) scdf_attr(data, .opt$phase) <- pvar
+  
   
   names(data) <- case_names
-  if (!is.null(names(scdfs))) 
-    names(data)[which(names(scdfs) != "")] <- names(scdfs)[which(names(scdfs) != "")]
+  if (!is.null(names(scdfs))) {
+    .names <- names(scdfs)[which(names(scdfs) != "")]
+    names(data)[which(names(scdfs) != "")] <- .names
+    
+  }
+  
+  if (!is.null(dvar)) source_attr[[.opt$dv]] <- dvar
+  if (!is.null(mvar)) source_attr[[.opt$mt]] <- mvar
+  if (!is.null(pvar)) source_attr[[.opt$phase]] <- pvar
   
   # check class scdf validity
   if (.opt$rigorous_class_check) {
