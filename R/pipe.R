32b59aab81c6b9afc1b64e7bd029b5b249c39728
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-06-25 11:33:29 +0200

    import and export %>%

---
+++ R/pipe.R
@@ -0,0 +1,10 @@
+#' Pipe
+#' 
+#' The plot functions in scan are designed to work with pipes \code{\%>\%}.
+#' This pipe function is directly imported from magrittr.
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+NULL
