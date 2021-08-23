4e2bb913ae4a1fa2eb2ecc9e4a24941c1a05a2f3
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-08-23 12:32:52 +0200

    set version to release candidate

--- R/private_options.R
+++ R/private_options.R
@@ -56,7 +56,7 @@
   function_deprecated_warning = "This function is deprecated. It might be dropped without any further notice in a future update of scan.",
   startup_message = paste0(
     "scan ", utils::packageVersion("scan"), 
-    " (", utils::packageDate('scan'), ")\n",
+    " release candidate (", utils::packageDate('scan'), ")\n",
     "Single-Case Data Analysis for Single and Multiple Baseline Designs\n"),
   style = list()
   )
