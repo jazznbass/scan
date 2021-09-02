b90940b44a784b781009891d215f9d493df98d50
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-09-02 14:06:45 +0200

    added example

--- R/select_phases.R
+++ R/select_phases.R
@@ -1,4 +1,4 @@
-#' Select phases for overlap analyses
+#' Select and combine phases for overlap analyses
 #' 
 #' Useful when working with %>% operators.
 #' 
@@ -7,10 +7,16 @@
 #' @param B Selection of the B phase
 #'
 #' @return An scdf with selected phases
+#' @examples 
+#' exampleA1B1A2B2 %>% 
+#'   select_phases(A = c(1, 3), B = c(2, 4)) %>%
+#'   overlap()
 #' @export
 select_phases <- function(data, A, B, ...) {
   data <- .prepare_scdf(data)
-  .keep_phases(data, phases = list(A = A, B = B))$data
-  
+  keep <- .keep_phases(data, phases = list(A = A, B = B))
+  data <- keep$data
+  scdf_attr(data, "phase_selection") <- list(A = keep$phases_A, B = keep$phases_B)
+  data
 }
 
