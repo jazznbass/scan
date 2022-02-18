552ea92c7ba1926ab8dc5d2ba4c6e343a01f4fff
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2022-02-18 08:52:26 +0100

    bew arguments to for selective alpha/beta/binomial tests

--- R/power_test.R
+++ R/power_test.R
@@ -8,20 +8,21 @@
 #' @inheritParams .inheritParams
 #' @param design An object created by design_rSC
 #' @param stat Defines the tests the power analysis is based on. The
-#' default \code{stat = c("plm_level", "rand", "tauU")} computes a power 
-#' analysis based on \code{\link{tau_u}}, \code{\link{randSC}} and 
-#' \code{\link{plm}} analyses. Further possibilities are: "plm_slope", 
-#' "plm_poisson_level", "plm_poisson_slope", "hplm_level", "hplm_slope", 
-#' "base_tau".
+#'   default \code{stat = c("plm_level", "rand", "tauU")} computes a power 
+#'   analysis based on \code{\link{tau_u}}, \code{\link{randSC}} and 
+#'   \code{\link{plm}} analyses. Further possibilities are: "plm_slope", 
+#'   "plm_poisson_level", "plm_poisson_slope", "hplm_level", "hplm_slope", 
+#'   "base_tau".
 #' @param effect Either "level" or "slope".
 #' @param n_sim Number of sample studies created for the the Monte-Carlo study.
-#' Default is \code{n = 100}. Ignored if design_is_one_study = FALSE.
+#'   Default is \code{n = 100}. Ignored if design_is_one_study = FALSE.
 #' @param design_is_one_study If TRUE, the design is assumed to define all 
-#' cases of one study that is repeatedly randomly created \code{n_sim} times. If false, 
-#' the design is assumed to contain all cases that from which a ransom sample is 
-#' generated. This is useful for very specific complex MC simulation studies.
-#' @param alpha Alpha level used to calculate the proportion of significant
-#' tests. Default is \code{alpha = 0.05}.
+#'   cases of one study that is repeatedly randomly created \code{n_sim} times. 
+#'   If false, the design is assumed to contain all cases that from which a 
+#'   random sample is generated. This is useful for very specific complex 
+#'   simulation studies.
+#' @param alpha_level Alpha level used to calculate the proportion of 
+#'   significant tests. Default is \code{alpha_level = 0.05}.
 #' @author Juergen Wilbert
 #' @seealso \code{\link{plm}}, \code{\link{randSC}}
 #' @examples
@@ -49,57 +50,82 @@ power_test <- function(design,
                        effect = "level",
                        n_sim = 100,
                        design_is_one_study = TRUE,
-                       alpha = 0.05) {
+                       alpha_stat = TRUE,
+                       power_stat = TRUE,
+                       binom_test = 0.5,
+                       alpha_level = 0.05) {
   
-  mc_fun <- .opt$mc_fun[which(names(.opt$mc_fun) %in% stat)]
-
-  res <- .power_test(
-    design = design, n_sim = n_sim, 
-    alpha = alpha, mc_fun = mc_fun, design_is_one_study = design_is_one_study
-  )
+  if (class(stat) == "list") {
+    if (class(stat[[1]]) == "function")
+    mc_fun <- stat
+  } 
   
+  if (class(stat) == "character") {
+    mc_fun <- .opt$mc_fun[which(names(.opt$mc_fun) %in% stat)]
+  } 
+  
+  # return object
   out <- data.frame(Method = names(mc_fun))
   
-  out$Power <- res * 100
+  # power calculation ----------
+  
+  if (power_stat) {
+    out$Power <- .power_test(
+      design = design, n_sim = n_sim, 
+      alpha_level = alpha_level, mc_fun = mc_fun, 
+      design_is_one_study = design_is_one_study
+    ) 
+  } else out$Power <- NA
+  
+  # alpha error calculation ----------
 
-  #level <- any(sapply(design$cases, function(x) x$level) != 0)
-  #slope <- any(sapply(design$cases, function(x) x$slope) != 0)
-  
-  design_no_effect <- design
-  if (effect == "level") {
-    design_no_effect$cases <- lapply(
-      design_no_effect$cases, 
-      function(x) {x$level <- rep(0, length = length(x$length)); x}
+  if (alpha_stat) {
+    
+    #level <- any(sapply(design$cases, function(x) x$level) != 0)
+    #slope <- any(sapply(design$cases, function(x) x$slope) != 0)
+    
+    design_no_effect <- design
+    if (effect == "level") {
+      design_no_effect$cases <- lapply(
+        design_no_effect$cases, 
+        function(x) {x$level <- rep(0, length = length(x$length)); x}
+      )
+    }
+    
+    if (effect == "slope") {
+      design_no_effect$cases <- lapply(
+        design_no_effect$cases, 
+        function(x) {x$slope <- rep(0, length = length(x$length)); x}
+      )
+    }
+    
+    out$"Alpha Error" <- .power_test(
+      design = design_no_effect, n_sim = n_sim, 
+      alpha_level = alpha_level, mc_fun = mc_fun, 
+      design_is_one_study = design_is_one_study
     )
-  }
+  } else out$"Alpha Error" <- NA
   
-  if (effect == "slope") {
-    design_no_effect$cases <- lapply(
-      design_no_effect$cases, 
-      function(x) {x$slope <- rep(0, length = length(x$length)); x}
-    )
-  }
-  
-  res <- .power_test(
-    design = design_no_effect, n_sim = n_sim, 
-    alpha = alpha, mc_fun = mc_fun, design_is_one_study = design_is_one_study
-  )
-  
-  out$"Alpha Error" <- res * 100
   out$"Alpha:Beta" <- sprintf("1:%.1f", (100 - out$Power) / out$Alpha)
   out$Correct <- (out$Power + (100 - out$"Alpha Error")) / 2
-  res <- sapply(
-    res, 
-    function(x) binom.test(round(x * n_sim * 2), n_sim * 2, p = 0.5)$p.value
-  )
-  out$p <- round(res, 3)
+  
+  if (!isFALSE(binom_test)) {
+    
+    b_test <- function(x) {
+      x <- binom.test(round(x / 100 * n_sim * 2), n_sim * 2, p = binom_test)
+      round(x$p.value, 3)
+    }
+    
+    out$p <- sapply(out$Correct, b_test)
+
+  }
 
   class(out) <- c("sc_power")
   out
 }
 
 .power_test <- function(design, 
-                        alpha = NA, 
+                        alpha_level = NA, 
                         n_sim, 
                         mc_fun, 
                         design_is_one_study) {
@@ -117,12 +143,14 @@ power_test <- function(design,
   }
   
   # analyse random sample ---------------------------------------------------
-  out <-  sapply(mc_fun, function(FUN) { 
-      p <- sapply(rand.sample, FUN)
-      mean(p <= alpha, na.rm = TRUE)
-    }
-  )
+  
+  test_function <- function(func) {
+    p <- sapply(rand.sample, func)
+    mean(p <= alpha_level, na.rm = TRUE) * 100
+  }
+  out <-  sapply(mc_fun, test_function) 
 
+  # return
   out
 }
 
