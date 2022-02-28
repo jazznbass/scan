445c54982977e1984b41b07c7cb3415ed6bb3388
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2022-02-28 10:18:21 +0100

    help page

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,5 +1,9 @@
 #' List of old deprecated function names
 #' 
+#' This is a list with functions names that have been replaced by new names
+#' which are in line with re R syntax guidelines. The old function names still 
+#' work. They are wrappers that call the new function.
+#' 
 #' @param ... Arguments passed through to the new function.
 #' @name deprecated-functions
 NULL
