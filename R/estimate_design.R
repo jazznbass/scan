4a16e3f5bae9d6a03da133d9159f9c7930d35f9f
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2022-03-25 17:35:04 +0100

    reworked the function

--- R/estimate_design.R
+++ R/estimate_design.R
@@ -1,38 +1,65 @@
 #' Estimate single-case design
 #'
 #' This functions takes an scdf and extracts design parameters. The resulting
-#' object can be unsed to randomly create new scdf files with the same
-#' underlying parameters. This is usefull for monte-carlo studies and
+#' object can be used to randomly create new scdf files with the same
+#' underlying parameters. This is useful for Monte-Carlo studies and
 #' bootstrapping procedures.
 #'
 #' @inheritParams .inheritParams
-#' @param m The mean depcting the overall distribution of which all cases are a random sample of.
-#' m is estimated when m = NULL.
-#' @param s The standard deviation depcting the between case variance of the overall performance.
+#' @param s The standard deviation depicting the between case variance of the overall performance.
 #' If more than two single-cases are included in the scdf, the variance is estimated if s is set to NULL.
 #' @param rtt The reliability of the measurements. The reliability is estimated when rtt = NULL.
-#' @param between If FALSE trend, level, and slope effect estimations will be identical for each case.
-#' If TRUE effects are estimated for each case seperately.
-#' @param ... Further arguments passed to the lm function used for parameter estimation.
+#' @param overall_rtt Ignored when `rtt` is set. If TRUE rtt estimations will be based on all cases and identical for each case.
+#' If FALSE rtt is estimated for each case separately.
+#' @param overall_effects If TRUE trend, level, and slope effect estimations will be identical for each case.
+#' If FALSE effects are estimated for each case separately.
+#' @param ... Further arguments passed to the plm function used for parameter estimation.
 #'
 #' @return A list of parameters for each single-case. Parameters include name, length, and
 #' starting measurementtime of each phase, trend level, and slope effects for each phase, mean,
 #' standarddeviation, and reliability for each case.
+#' @examples 
+#' # create a random scdf with predefined parameters
+#' set.seed(1234)
+#' design <- design(
+#'   n = 10, trend = -0.02, 
+#'   level = list(0, 1), rtt = 0.8,
+#'   s = 1
+#'   )
+#' scdf<- random_scdf(design)
+#' 
+#' # Estimate the parameters based on the scdf and create a new random scdf 
+#' # based on these estimations
+#' design_est <- estimate_design(scdf, rtt = 0.8)
+#' scdf_est <- random_scdf(design_est)
+#' 
+#' # Analyze both datasets with an hplm model. See how similar the estimations are:
+#' hplm(scdf, slope = FALSE)
+#' hplm(scdf_est, slope = FALSE)
+#' 
+#' # Also simar results for pand and randomization tests:
+#' pand(scdf)
+#' pand(scdf_est)
+#' rand_test(scdf)
+#' rand_test(scdf_est)
 #' @export
 
 estimate_design <- function(data, dvar, pvar, mvar, 
-                            m = NULL, 
                             s = NULL, 
                             rtt = NULL, 
-                            between = TRUE, 
+                            overall_effects = FALSE, 
+                            overall_rtt = TRUE,
                             model = "JW", 
                             ...) {
 
   # set attributes to arguments else set to defaults of scdf
-  if (missing(dvar)) dvar <- scdf_attr(data, .opt$dv) else scdf_attr(data, .opt$dv) <- dvar
-  if (missing(pvar)) pvar <- scdf_attr(data, .opt$phase) else scdf_attr(data, .opt$phase) <- pvar
-  if (missing(mvar)) mvar <- scdf_attr(data, .opt$mt) else scdf_attr(data, .opt$mt) <- mvar
-
+  if (missing(dvar)) dvar <- scdf_attr(data, .opt$dv)
+  if (missing(pvar)) pvar <- scdf_attr(data, .opt$phase)
+  if (missing(mvar)) mvar <- scdf_attr(data, .opt$mt)
+  scdf_attr(data, .opt$dv) <- dvar
+  scdf_attr(data, .opt$phase) <- pvar
+  scdf_attr(data, .opt$mt) <- mvar
+  
   data <- .prepare_scdf(data)
   N <- length(data)
   case_names <- names(data)
@@ -40,7 +67,7 @@ estimate_design <- function(data, dvar, pvar, mvar,
   if (is.null(case_names)) case_names <- .case_names(names(data), length(data))
 
   cases <- lapply(data, function(x) {
-    df <- as.data.frame(.phasestructure(x, pvar))
+    df <- as.list(.phasestructure(x, pvar))
     names(df)[1:2] <- c("length", "phase")
     df
   })
@@ -49,36 +76,43 @@ estimate_design <- function(data, dvar, pvar, mvar,
   fitted <- c()
 
   for (i in 1:N) {
-    plm.model <- plm(data[i], model = model, ...)$full
-    res <- coef(plm.model)
-    var_fitted <- var(plm.model$fitted.values)
-    var_residuals <- var(plm.model$residuals)
-    n.phases <- nrow(cases[[i]])
-    cases[[i]]$m <- res[1]
+    plm_model <- plm(data[i], model = model, ...)$full
+    res <- coef(plm_model)
+    var_fitted <- var(plm_model$fitted.values)
+    var_residuals <- var(plm_model$residuals)
+    n_phases <- length(cases[[i]]$phase)
+    cases[[i]]$start_value <- res[1]
     cases[[i]]$trend <- res[2]
-    cases[[i]]$level <- c(0, res[3:(1 + n.phases)])
-    cases[[i]]$slope <- c(0, res[(2 + n.phases):(2 + 2 * (n.phases - 1))])
-    cases[[i]]$error <- var_residuals
-    cases[[i]]$fitted <- var_fitted
-    cases[[i]]$rtt <- var_fitted / (var_fitted + var_residuals)
-
-
-    missing.p <- c()
-    for (y in 1:nrow(cases[[i]])) {
-      tmp <- cases[[i]][y, ]
-      missing.p <- c(
-        missing.p, 
-        sum(is.na(data[[i]][tmp$start:tmp$stop, dvar])) / tmp$length
-      )
+    cases[[i]]$level <- c(0, res[3:(1 + n_phases)])
+    cases[[i]]$slope <- c(0, res[(2 + n_phases):(2 + 2 * (n_phases - 1))])
+    
+    if (is.null(rtt)) {
+      cases[[i]]$rtt <- var_fitted / (var_fitted + var_residuals)
+    } else {
+      cases[[i]]$rtt <- rtt
     }
-    cases[[i]]$missing.p <- missing.p
-    error <- c(error, plm.model$residuals)
-    fitted <- c(fitted, plm.model$fitted.values)
+    
+    cases[[i]]$missing_prop <- mean(is.na(data[[i]][[dvar]]))
+    
+    #missing_prop <- c()
+    # for (y in 1:length(cases[[i]]$phase)) {
+    #   missing_prop <- c(
+    #     missing_prop, 
+    #     sum(is.na(data[[i]][cases[[i]]$start[y]:cases[[i]]$stop[y], dvar])) / cases[[i]]$length[y]
+    #   )
+    # }
+    #cases[[i]]$error <- var_residuals
+    #cases[[i]]$fitted <- var_fitted
+    
+    error <- c(error, plm_model$residuals)
+    fitted <- c(fitted, plm_model$fitted.values)
   }
 
-  if (!between) {
+  if (N > 2 && is.null(s)) s <- sd(sapply(cases, function(x) x$start_value[1]), na.rm = TRUE)
+  
+  if (overall_effects) {
     level <- rowMeans(sapply(cases, function(x) x$level))
-    trend <- rowMeans(sapply(cases, function(x) x$trend))
+    trend <- mean(sapply(cases, function(x) x$trend))
     slope <- rowMeans(sapply(cases, function(x) x$slope))
     for (i in 1:N) {
       cases[[i]]$trend <- trend
@@ -86,19 +120,10 @@ estimate_design <- function(data, dvar, pvar, mvar,
       cases[[i]]$slope <- slope
     }
   }
-
-  if (is.null(m)) m <- mean(sapply(cases, function(x) x$m[1]), na.rm = TRUE)
-  if (N > 2 && is.null(s)) s <- sd(sapply(cases, function(x) x$m[1]), na.rm = TRUE)
-
-  # if (is.null(rtt)) rtt <- 1 - (error / s^2)
-
-  if (is.null(rtt)) rtt <- var(fitted) / (var(fitted) + var(error))
-
-  error_sd <- sqrt(((1 - rtt) / rtt) * s^2)
-
+  
   vars <- c(
-    "phase", "length", "mt", "rtt", "error", "missing.p", "extreme.p", 
-    "extreme.low", "extreme.high", "trend", "level", "slope", "m", "s", 
+    "phase", "length", "rtt", "missing_prop", "extreme_prop", 
+    "extreme_low", "extreme_high", "trend", "level", "slope", "start_value", "s", 
     "start", "stop"
   )
 
@@ -106,20 +131,17 @@ estimate_design <- function(data, dvar, pvar, mvar,
     cases[[i]]$level <- cases[[i]]$level / s
     cases[[i]]$slope <- cases[[i]]$slope / s
     cases[[i]]$trend <- cases[[i]]$trend / s
-    cases[[i]]$error <- error_sd
     cases[[i]]$s <- s
-    cases[[i]]$rtt <- rtt
-    cases[[i]]$extreme.p <- 0
-    cases[[i]]$extreme.low <- -4
-    cases[[i]]$extreme.high <- -3
-    cases[[i]]$mt <- sum(cases[[i]]$length)
+    if (overall_rtt) cases[[i]]$rtt <- var(fitted) / (var(fitted) + var(error))
+    cases[[i]]$extreme_prop <- 0
+    cases[[i]]$extreme_low <- -4
+    cases[[i]]$extreme_high <- -3
     cases[[i]] <- cases[[i]][vars]
   }
 
   out <- list(
     cases = cases,
-    distribution = "normal",
-    prob = NA
+    distribution = "normal"
   )
 
   class(out) <- c("sc_design")
