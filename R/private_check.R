4545cd87255865052e690392dece0c21231c98d4
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2022-03-29 15:06:48 +0200

    refinded check within

--- R/private_check.R
+++ R/private_check.R
@@ -15,18 +15,21 @@
 }
 
 .check_in <- function(x, arg, match) {
-  
   msg <- paste0("'", match, "'")
-  if (length(match) == 1) msg <- msg
   if (length(match) == 2) msg <- paste0(msg, collapse = " or ")
-  if (length(match) > 2) 
-    msg <- paste0("one of ", paste0(msg, collapse = ", "))
-  .check(x, arg %in% match, as.character(match.call()[3]), " not ", msg, ".")
+  if (length(match) > 2) msg <- paste0("one of ", paste0(msg, collapse = ", "))
+  .check(x, arg %in% match, as.character(match.call()[3]), " is not ", msg, ".")
 }
 
 .check_within <- function(x, arg, lower, upper) {
-  x <- .check_not(x, any(arg<lower), as.character(match.call()[3]), " < ", lower) 
-  x <- .check_not(x, any(arg>upper), as.character(match.call()[3]), " > ", upper) 
+  if (!missing(lower))
+    x <- .check_not(
+      x, any(arg < lower), as.character(match.call()[3]), " < ", lower
+    ) 
+  if (!missing(upper))
+    x <- .check_not(
+      x, any(arg > upper), as.character(match.call()[3]), " > ", upper
+    ) 
   x
 }
 
