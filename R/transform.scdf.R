07148cec035ccb1aaf220525ddb74ca2a5f72347
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-02-04 09:00:12 +0100

    added the all() helper function

--- R/transform.scdf.R
+++ R/transform.scdf.R
@@ -6,6 +6,11 @@
 #' This function is a method of the generic transformation function. Unlike this
 #' one, expressions are calculated serially. This means that the results of the
 #' calculation of an expression are the basis for the following calculations.
+#' The `all` function is a helper function to calculate values across all 
+#' cases. It also takes an expression as an argument. 
+#' E.g., `mean(all(values))` will calculate the mean of values across all 
+#' cases. `mean(all(values[phase == "A"]))` will calculate the mean of 
+#' values where phase is A across all cases.
 #'
 #' @param _data A scdf.
 #' @param ... Expressions.
@@ -31,14 +36,38 @@
 #'     mean_values = mean(values), 
 #'     sd_values = sd(values)
 #'   )
+#'
+#' ## Use `all` to calculate global variables.   
+#' exampleAB %>% 
+#'   transform(
+#'     values_center_case = values - mean(values[phase == "A"]), 
+#'     values_center_global = values - mean(all(values[phase == "A"])),
+#'     value_dif = values_center_case - values_center_global
+#'   )
+#'
+#' ## Standardize the values by the overall mean and standarddeviation
+#' exampleABC %>%
+#'   transform(values = (values - mean(all(values))) / sd(all(values)))
+
 
 transform.scdf <- function(`_data`, ...) {
   f <- substitute(list(...))
+  df_all <- as.data.frame(`_data`)
+  
   for(i in seq_along(`_data`)) {
+    .list <- as.list(`_data`[[i]])
+    .list$all_cases <- .list$all <- function(x) {
+      x <- substitute(x)
+      eval(x, df_all)
+    }
+    
     for(j in 2:length(f)) {
-      new <- eval(f[c(1,j)], `_data`[[i]], parent.frame())
+      new <- eval(f[c(1,j)], .list, parent.frame())
       `_data`[[i]][[names(new)]] <- new[[1]]
+      .list[[names(new)]] <- new[[1]]
+      
     }
   }
   `_data`
 }
+
