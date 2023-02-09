037c6c611d3dda24d045f739cd104395107fb2c0
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-02-09 12:23:30 +0100

    across_cases() and all_cases() extended examples.

--- R/transform.scdf.R
+++ R/transform.scdf.R
@@ -1,20 +1,24 @@
 #' Transform variables in every single case of a single case data frame
 #'
-#' Takes a scdf and applies transformations to each individual case. This is
+#' Takes an scdf and applies transformations to each individual case. This is
 #' useful to calculate or modify new variables.
 #'
 #' This function is a method of the generic transform function. Unlike the
 #' generic function, it calculates expressions serially. This means that the
 #' results of the calculation of one expression are the basis for the following
-#' computations. The 'all' function is a helper function that calculates values
-#' across all cases. It also takes an expression as an argument. For example,
-#' \code{mean(all(values))} calculates the mean of the values across all cases.
-#' \code{mean(all(values[phase == "A"]))} will calculate the mean of all values
-#' where phase is A.
+#' computations. The \code{all_cases} function is a helper function that
+#' extracts the values of a variable from all cases. It takes an expression as
+#' an argument. For example, \code{mean(all_cases(values))} calculates the mean
+#' of the values from all cases. \code{mean(all_cases(values[phase == "A"]))}
+#' will calculate the mean of all values where phase is A. The function
+#' \code{across_cases} allows to calculate new variables or replace existing
+#' variables across all
+#' cases. E.g., \code{across_cases(values_ranked = rank(values, na.last =
+#' "keep"))} will calculate a new variable with values ranked across all cases.
 #'
-#' @param _data A scdf.
+#' @param _data An scdf.
 #' @param ... Expressions.
-#' @return A scdf.
+#' @return An scdf.
 #' @export
 #' @examples
 #' ## Creates a single-case with frequency distributions. The proportion and
@@ -45,9 +49,19 @@
 #'     value_dif = values_center_case - values_center_global
 #'   )
 #'
-#' ## Standardize the values by the overall mean and standarddeviation
+#' ## Use `across_cases` to calculate or replace a variable with values from
+#' ## all cases. E.g., standardize the dependent variable:
 #' exampleABC %>%
-#'   transform(values = (values - mean(all(values))) / sd(all(values)))
+#'   transform(
+#'     across_cases(values = scale(values))
+#'   )
+#'
+#' ## Rank transform the values based on all cases vs. within each case:
+#' exampleABC %>%
+#'   transform(
+#'     across_cases(values_across = rank(values, na.last="keep")),
+#'     value_within = rank(values, na.last="keep")
+#'   )
 #'
 #' ## Three helper functions to smooth the data
 #' Huber2014$Berta %>%
@@ -57,63 +71,50 @@
 #'   "compliance (local regression)" = local_regression(compliance, mt)
 #' )
 #'
-#' ## Function invalidate() helps to set NAs for specific phases.
-#' ## E.g., you want to delete the first two values of phase A and the first
+#' ## Function first_of() helps to set NAs for specific phases.
+#' ## E.g., you want to replace the first two values of phase A and the first
 #' ## value of phase B and its preceding value.
 #'
 #' byHeart2011 %>%
 #'   transform(
-#'     values = invalidate(values, phase == "A", 0:1),
-#'     values = invalidate(values, phase == "B", -1:0)
+#'     values = replace(values, first_of(phase == "A", 0:1), NA),
+#'     values = replace(values, first_of(phase == "B", -1:0), NA)
 #'   )
+#' @export
 transform.scdf <- function(`_data`, ...) {
-  f <- substitute(list(...))
-  df_all <- as.data.frame(`_data`)
+  expressions <- substitute(list(...))
+  .df <- as.data.frame(`_data`)
   
-  for(i in seq_along(`_data`)) {
-    .list <- as.list(`_data`[[i]])
-    .list$all_cases <- .list$all <- function(x) {
-      x <- substitute(x)
-      eval(x, df_all)
-    }
-    
-    for(j in 2:length(f)) {
-      new <- eval(f[c(1,j)], .list, parent.frame())
-      `_data`[[i]][[names(new)]] <- new[[1]]
-      .list[[names(new)]] <- new[[1]]
-      
+  for(i_expression in 2:length(expressions)) {
+    if(startsWith(deparse(expressions[[i_expression]]), "across_cases(")) {
+      .list_env <- as.list(.df)
+      .list_env$across_cases <- function(...) {
+        exp_across <- substitute(list(...))
+        for (i in 2:length(exp_across)) {
+          new <- eval(exp_across[c(1,i)], .list_env)
+          .df[[names(new)]] <- new[[1]]
+          .list_env[[names(new)]] <- new[[1]]
+        }
+        .df
+      }
+      new <- eval(expressions[c(1,2)], .list_env, parent.frame())
+      .df <- new[[1]]
+      `_data` <- suppressMessages(as_scdf(.df))
+    } else {
+      for(i_case in seq_along(`_data`)) {
+        .list_env <- as.list(`_data`[[i_case]])
+        .list_env$all_cases <- .list_env$all <- function(x) {
+          x <- substitute(x)
+          eval(x, .df)
+        }
+        new <- eval(expressions[c(1,i_expression)], .list_env, parent.frame())
+        `_data`[[i_case]][[names(new)]] <- new[[1]]
+      }
     }
   }
   `_data`
 }
 
-#' @export
-#' @rdname transform.scdf
-#' @param x A vector.
-#' @param first_of A logical vector. The first appearance of a TRUE value is the 
-#'   reference position.
-#' @param positions A numeric vector with relative positions to the first 
-#'   appearance of a TRUE value in filter.
-invalidate <- function(x, first_of, positions) {
-  x[match(TRUE, first_of) + positions] <- NA
-  x
-}
 
-#' @rdname transform.scdf
-#' @param lag Number of values surrounding a value to calculate the average
-#' @export
-moving_median <- function(x, lag = 1) .moving_average(x, lag, median)
 
-#' @rdname transform.scdf
-#' @param lag Number of values surrounding a value to calculate the average
-#' @export
-moving_mean <- function(x, lag = 1) .moving_average(x, lag, mean)
-
-#' @rdname transform.scdf
-#' @param f the proportion of surrounding data influencing each data point.
-#' @param mt A vector with measurement times.
-#' @export
-local_regression <- function(x, f = 0.2, mt = 1:length(x)) {
-  lowess(x ~ mt, f = f)$y
-}
 
