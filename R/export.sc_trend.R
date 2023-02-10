26ecb07530a42bf859626ac61b7c4a0eef005fcc
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-02-10 17:46:43 +0100

    replaced offset with first_mt. cleaned code, helpfile

---
+++ R/export.sc_trend.R
@@ -0,0 +1,50 @@
+#' @rdname export
+#' @export
+export.sc_trend <- function(object, 
+                            caption = NA, 
+                            footnote = NA, 
+                            filename = NA,
+                            kable_styling_options = list(), 
+                            kable_options = list(), 
+                            round = 2,
+                            flip = FALSE,
+                            ...) {
+  
+  kable_options <- .join_kabel(kable_options)
+  kable_styling_options <- .join_kabel_styling(kable_styling_options)
+  
+  if (is.na(caption)) caption <- c("Trend analysis")
+  kable_options$caption <- caption
+  
+  out <- object$trend
+  if (isTRUE(flip)) out <- t(out)
+  
+  tmp.rownames <- rownames(out)
+  rownames(out) <- NULL
+  for (tmp in object$formulas) {
+    tmp.rownames <- gsub(paste0(tmp, "."), "", tmp.rownames)
+  }
+  out <- cbind(Phase = tmp.rownames, out)
+  
+  kable_options$x <- out
+  table <- do.call(kable, kable_options)
+  kable_styling_options$kable_input <- table
+  table <- do.call(kable_styling, kable_styling_options)
+  
+  for (i in 1:length(object$formulas)) {
+    table <- group_rows(
+      table, object$formulas[i],
+      1 + (i - 1) * (length(object$design) + 1),
+      i * (length(object$design) + 1)
+      #label_row_css = "text-align: center;"
+    )
+  }
+  
+  if (!is.na(footnote) && footnote != "") 
+    table <- footnote(table, general = footnote, threeparttable = TRUE)
+  
+  # finish ------------------------------------------------------------------
+  
+  if (!is.na(filename)) cat(table, file = filename)
+  table
+}
--- R/trend.R
+++ R/trend.R
@@ -1,50 +1,57 @@
 #' Trend analysis for single-cases data
-#' 
-#' The \code{trend} function provides an overview of linear trends in
-#' single-case data.  By default, it gives you the intercept and slope of a
-#' linear and a squared regression of measurement-time on scores.  Models are
-#' computed separately for each phase and across all phases. For a
-#' more advanced application, you can add regression models using the R
-#' specific formula class.
-#' 
+#'
+#' The \code{trend} function provides an overview of linear trends in single
+#' case data. By default, it provides the intercept and slope of a linear and
+#' quadratic regression of measurement time on scores. Models are calculated
+#' separately for each phase and across all phases. For more advanced use, you
+#' can add regression models using the R-specific formula class.
+#'
 #' @inheritParams .inheritParams
-#' @param offset An offset for the first measurement-time of each phase (MT). If
-#' \code{offset = 0}, the phase measurement is handled as MT 1.
-#' Default is \code{offset = -1}, setting the first value of MT to 0.
+#' @param first_mt A numeric setting the value for the first measurement-time.
+#'   Default = 0.
+#' @param offset (Deprecated. Please use first_mt). An offset for the first
+#'   measurement-time of each phase. If \code{offset = 0}, the phase measurement
+#'   is handled as MT 1. Default is \code{offset = -1}, setting the first value
+#'   of MT to 0.
 #' @param model A string or a list of (named) strings each depicting one
-#' regression model. This is a formula expression of the standard R class. The
-#' parameters of the model are \code{values}, \code{mt} and \code{phase}.
+#'   regression model. This is a formula expression of the standard R class. The
+#'   parameters of the model are \code{values}, \code{mt} and \code{phase}.
 #' @return \item{trend}{A matrix containing the results (Intercept, B and beta)
-#' of separate regression models for phase A, phase B, and the whole data.}
+#'   of separate regression models for phase A, phase B, and the whole data.}
 #' \item{offset}{Numeric argument from function call (see \code{Arguments}
 #' section).}
 #' @author Juergen Wilbert
-#' @seealso \code{\link{describe}}, \code{\link{autocorr}},
-#' \code{\link{plm}}
+#' @seealso \code{\link{describe}}, \code{\link{autocorr}}, \code{\link{plm}}
 #' @examples
-#' 
+#'
 #' ## Compute the linear and squared regression for a random single-case
 #' design <- design(slope = 0.5)
 #' matthea <- random_scdf(design)
 #' trend(matthea)
-#' 
+#'
 #' ## Besides the linear and squared regression models compute two custom models:
 #' ## a) a cubic model, and b) the values predicted by the natural logarithm of the
 #' ## measurement time.
 #' design <- design(slope = 0.3)
 #' ben <- random_scdf(design)
 #' trend(ben, offset = 0, model = c("Cubic" = values ~ I(mt^3), "Log Time" = values ~ log(mt)))
-#' 
+#'
 #' @export
-trend <- function(data, dvar, pvar, mvar, offset = -1, model = NULL) {
+trend <- function(data, dvar, pvar, mvar, 
+                  offset = "deprecated",
+                  first_mt = 0,
+                  model = NULL) {
 
+  
+  if (is.numeric(offset)) first_mt <- offset + 1
+  
   # set attributes to arguments else set to defaults of scdf
-  if (missing(dvar)) 
-    dvar <- scdf_attr(data, .opt$dv)    else scdf_attr(data, .opt$dv)    <- dvar
-  if (missing(pvar)) 
-    pvar <- scdf_attr(data, .opt$phase) else scdf_attr(data, .opt$phase) <- pvar
-  if (missing(mvar)) 
-    mvar <- scdf_attr(data, .opt$mt)    else scdf_attr(data, .opt$mt)    <- mvar
+  if (missing(dvar)) dvar <- scdf_attr(data, .opt$dv)
+  if (missing(pvar)) pvar <- scdf_attr(data, .opt$phase)
+  if (missing(mvar)) mvar <- scdf_attr(data, .opt$mt)
+  scdf_attr(data, .opt$dv)    <- dvar
+  scdf_attr(data, .opt$phase) <- pvar
+  scdf_attr(data, .opt$mt)    <- mvar
   
   data <- .prepare_scdf(data)
 
@@ -71,41 +78,39 @@ trend <- function(data, dvar, pvar, mvar, offset = -1, model = NULL) {
     formula(paste0(dvar, " ~ ", mvar)) , 
     formula(paste0(dvar, " ~ I(", mvar, "^2)"))
   )
-  fomulas_names <- c("Linear", "Squared")
+  fomulas_names <- c("Linear", "Quadratic")
   if(!is.null(model)) {
     fomulas <- c(fomulas, model)
     fomulas_names <- c(fomulas_names, names(model))
   }
   tmp <- length(design) + 1
-  rows <- paste0( paste0(rep(fomulas_names, each = tmp), ".") ,c("ALL", design))
+  rows <- paste0(paste0(rep(fomulas_names, each = tmp), "."), c("ALL", design))
   
   ma <- matrix(NA, nrow = length(rows), ncol = 3)
   row.names(ma) <- rows
   colnames(ma) <- c("Intercept", "B", "Beta")
   ma <- as.data.frame(ma)
   
-  for(f in 1:length(fomulas)) {
-    var <- paste0(fomulas_names[f], ".ALL")
-    data.phase <- data
-    data.phase[, mvar] <- data.phase[, mvar] - 
-                          min(data.phase[, mvar], na.rm = TRUE) + 1 + offset
-    ma[which(rows == var), 1:3] <- .beta_weights(
-      lm(fomulas[[f]], data = data.phase)
-    )
-    for(p in 1: length(design)) {
-      data.phase <- data[phases$start[p]:phases$stop[p], ]
-      data.phase[, mvar] <- data.phase[,mvar] - 
-                            min(data.phase[, mvar], na.rm = TRUE) + 1 + offset
-      var <- paste0(fomulas_names[f], ".", design[p])
-      ma[which(rows == var), 1:3] <- .beta_weights(
-        lm(fomulas[[f]], data = data.phase)
-      )
+  for(i_formula in 1:length(fomulas)) {
+    data_phase <- data
+    mvar_correction <- min(data_phase[[mvar]], na.rm = TRUE) - first_mt
+    data_phase[[mvar]] <- data_phase[[mvar]] - mvar_correction
+    
+    .row <- which(rows == paste0(fomulas_names[i_formula], ".ALL"))
+    ma[.row, 1:3] <- .beta_weights(lm(fomulas[[i_formula]], data = data_phase))
+    for(p in 1:length(design)) {
+      data_phase <- data[phases$start[p]:phases$stop[p], ]
+      mvar_correction <- min(data_phase[[mvar]], na.rm = TRUE) - first_mt
+      data_phase[[mvar]] <- data_phase[[mvar]] - mvar_correction 
+      .row <- which(rows == paste0(fomulas_names[i_formula], ".", design[p]))
+      ma[.row, 1:3] <- .beta_weights(lm(fomulas[[i_formula]], data=data_phase))
     }
   }
   
   out <- list(
     trend = ma, 
     offset = offset, 
+    first_mt = first_mt,
     formulas = fomulas_names, 
     design = design
   )
