6d8af5cf41163097508d2b9cdfb304b1eabf95e7
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-03-27 16:47:13 +0200

    moved to misc

---
+++ misc/show.R
@@ -0,0 +1,30 @@
+#' Show scdf cases in viewer
+#' 
+#' Each case with in scdf opens a separate viewer tab.
+#'
+#' @param scdf A scdf object
+#' @param max Maximum number of cases opened
+#' @export
+#'
+#' @examples
+#' ## show(exampleABC)
+show <- function(scdf, max = 5) {
+  if (length(scdf) > max) {
+    cat("To many cases to view. Set argument 'max' to a higher value.")
+    return()
+  }
+  
+  var_dv <- scdf_attr(scdf, opt("dv"))
+  var_phase <- scdf_attr(scdf, opt("phase"))
+  var_mt <- scdf_attr(scdf, opt("mt"))
+  
+  mapply(function(x, name) {
+      attr(x[[var_dv]], "label") <- "Dependent variable"
+      attr(x[[var_mt]], "label") <- "Measurement time"
+      attr(x[[var_phase]], "label") <- "Phase"
+      invisible(utils::View(x, name))
+    }, 
+    scdf, names(scdf)
+  )
+  
+}
