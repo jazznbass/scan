0bd2d1ee8f862ed94d3b671cb335b76b34bb8ab9
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-09-04 09:14:45 +0200

    new select argument and function

--- R/export.sc_pem.R
+++ R/export.sc_pem.R
@@ -1,23 +1,19 @@
 #' @rdname export
 #' @export
 export.sc_pem <- function(object, 
-                            caption = NA, 
-                            footnote = NA, 
-                            filename = NA,
-                            kable_styling_options = list(), 
-                            kable_options = list(), 
-                            round = 2,
-                            ...) {
+                          caption = NA, 
+                          footnote = NA, 
+                          filename = NA,
+                          kable_styling_options = list(), 
+                          kable_options = list()) {
   
   kable_options <- .join_kabel(kable_options)
   kable_styling_options <- .join_kabel_styling(kable_styling_options)
   
   if (is.na(caption)) caption <- c("Percent Exceeding the Median")
   
-  out <- cbind(PEM = object$PEM, object$test)
-    
   table <- .create_table(
-    out, 
+    object$PEM, 
     kable_options, 
     kable_styling_options, 
     caption = caption,
---
+++ R/export.sc_pet.R
@@ -0,0 +1,48 @@
+#' @rdname export
+#' @export
+export.sc_pet <- function(object, 
+                          caption = NA, 
+                          footnote = NA, 
+                          filename = NA,
+                          kable_styling_options = list(), 
+                          kable_options = list()) {
+  
+  kable_options <- .join_kabel(kable_options)
+  kable_styling_options <- .join_kabel_styling(kable_styling_options)
+  
+  if (is.na(caption)) caption <- c("Percent Exceeding the trend")
+  
+  if (is.na(footnote)) {
+    if (object$decreasing) {
+      footnote <- c(
+        "Assumed decreasing values in the B-phase.",
+        "Binom.test: alternative hypothesis: true probability < 50%",
+        sprintf(
+          "PET CI: Percent of values less than lower %d%% confidence threshold (smaller %.3f*se below predicted value)", 
+          object$ci.percent, object$se.factor
+        )
+      )
+    } else {
+      footnote <- c(
+        "Binom.test: alternative hypothesis: true probability > 50%",
+        sprintf(
+          "PET CI: Percent of values greater than upper %d%% confidence threshold (greater %.3f*se above predicted value)", 
+          object$ci.percent, object$se.factor
+        )
+      )
+    }
+  }
+  
+  table <- .create_table(
+    object$PET, 
+    kable_options, 
+    kable_styling_options, 
+    caption = caption,
+    footnote = footnote
+  )
+  
+  # finish ------------------------------------------------------------------
+  
+  if (!is.na(filename)) .save_export(table, filename)
+  table
+}
