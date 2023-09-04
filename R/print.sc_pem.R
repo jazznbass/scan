3a3407968e5b1f9dd2bec4d536bbfdaa9a1ce1b7
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-09-04 09:16:23 +0200

    optimization

--- R/print.sc_pem.R
+++ R/print.sc_pem.R
@@ -3,8 +3,7 @@
 #' 
 print.sc_pem <- function(x, ...) {
   cat("Percent Exceeding the Median\n\n")
-  ma <- cbind(PEM = x$PEM, x$test)
-  print(round(ma, 3))
+  print(x$PEM, digits = 3, row.names = FALSE)
   cat("\n")
   if (x$decreasing) {
     cat("Assumed decreasing values in the B-phase.\n\n")
