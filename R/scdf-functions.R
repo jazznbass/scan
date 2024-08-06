a7de12ab35221be0797d192b3d0ec0d93ba7b881
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2024-08-06 15:35:17 +0200

    method for na.omit()

--- R/scdf-functions.R
+++ R/scdf-functions.R
@@ -104,3 +104,14 @@ c.scdf <- function(...) {
 #' @export
 is.scdf <- function(x) inherits(x, "scdf")
 
+#' scdf objects
+#' Removes any row with a missing value
+#'
+#' @param object A scdf.
+#' @return A scdf object.
+#' @export
+na.omit.scdf <- function(object, ...) {
+  for (i in 1:length(object))
+    object[[i]] <- na.omit(object[[i]])
+  object
+}
