a4b81c98bb4876f7a82ee12fdeb770eca1610ec9
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2025-08-28 18:49:38 +0200

    hplm

--- tests/testthat/test-hplm.R
+++ tests/testthat/test-hplm.R
@@ -9,6 +9,16 @@ test_that("hplm", {
   values <- round(sum(residuals(result$hplm)^2), 3)
   expect_equal(values, 3005.251)
   
-  
+  result <- hplm(
+    exampleAB_50, 
+    model = "W",
+    method = "REML",
+    unequal_variances = TRUE,
+    random_trend = TRUE,
+    control = lmeControl(msMaxIter = 50, apVar = FALSE, returnObject = TRUE),
+    ar = 1
+  )
+  values <- round(sum(residuals(result$hplm)^2))
+  expect_equal(values, 34923)
   
 })
